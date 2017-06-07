ca7499340cdf20a7bb0f061b8f76dc705bdb4299
HAN, CHIHUN <sue_z@naver.com>
2017-06-07 11:08:55 +0900

    견적서 리스트 구현중

--- BikeProject/src/main/java/com/sist/estimate/FrameVO.java
+++ BikeProject/src/main/java/com/sist/estimate/FrameVO.java
@@ -3,15 +3,15 @@ package com.sist.estimate;
   CREATE TABLE product_frame (
   p_no NUMBER PRIMARY KEY ,
   p_name VARCHAR2(50) UNIQUE ,
-  p_price VARCHAR2(30) NOT NULL ,
-  p_regdate VARCHAR2(5) NOT NULL
+  p_price NUMBER NOT NULL ,
+  p_regdate NUMBER NOT NULL
 );
  */
 public class FrameVO {
 	private int fno;
 	private String fname;
 	private int fprice;
-	private String fregdate;
+	private int fregdate;
 	
 	public int getFno() {
 		return fno;
@@ -31,10 +31,10 @@ public class FrameVO {
 	public void setFprice(int fprice) {
 		this.fprice = fprice;
 	}
-	public String getFregdate() {
+	public int getFregdate() {
 		return fregdate;
 	}
-	public void setFregdate(String fregdate) {
+	public void setFregdate(int fregdate) {
 		this.fregdate = fregdate;
 	}
 }
